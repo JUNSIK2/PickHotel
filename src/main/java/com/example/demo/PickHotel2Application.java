984e2520a0a4dd594f6c7dababf97fc49c8c4740
JJH <arc551234@gmail.com>
2021-12-23 14:28:35 +0900

    pickHotel

---
+++ src/main/java/com/example/demo/PickHotel2Application.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.DispatcherServlet;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.example"})
+public class PickHotel2Application extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(PickHotel2Application.class, args);
+		DispatcherServlet dispatcherServlet = (DispatcherServlet)ctx.getBean("dispatcherServlet");
+		dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
+	}
+
+}
